857341f950c28e67418ea8bcad8d4f01a039937d
Random <78389553+Sam772@users.noreply.github.com>
2024-03-18 02:33:50 +0000

    Again

--- src/pages/WorkspaceCreation.tsx
+++ src/pages/WorkspaceCreation.tsx
@@ -1,8 +1,75 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './WorkspaceCreation.css';
+import { PaletteMode } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import AutoAwesomeRoundedIcon from '@mui/icons-material/AutoAwesomeRounded';
+import AppAppBar from '../components/AppAppBar';
+import Hero from '../components/Hero';
+import getLPTheme from '../getLPTheme';
+
+interface ToggleCustomThemeProps {
+  showCustomTheme: Boolean;
+  toggleCustomTheme: () => void;
+}
+
+function ToggleCustomTheme({
+  showCustomTheme,
+  toggleCustomTheme,
+}: ToggleCustomThemeProps) {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '100dvw',
+        position: 'fixed',
+        bottom: 24,
+      }}
+    >
+      <ToggleButtonGroup
+        color="primary"
+        exclusive
+        value={showCustomTheme}
+        onChange={toggleCustomTheme}
+        aria-label="Platform"
+        sx={{
+          backgroundColor: 'background.default',
+          '& .Mui-selected': {
+            pointerEvents: 'none',
+          },
+        }}
+      >
+        <ToggleButton value>
+          <AutoAwesomeRoundedIcon sx={{ fontSize: '20px', mr: 1 }} />
+          Custom theme
+        </ToggleButton>
+        <ToggleButton value={false}>Material Design 2</ToggleButton>
+      </ToggleButtonGroup>
+    </Box>
+  );
+}
 
 const WorkspaceCreation: React.FC = () => {
+
+  const [mode, setMode] = React.useState<PaletteMode>('light');
+  const [showCustomTheme, setShowCustomTheme] = React.useState(true);
+  const LPtheme = createTheme(getLPTheme(mode));
+  const defaultTheme = createTheme({ palette: { mode } });
+
+  const toggleColorMode = () => {
+    setMode((prev) => (prev === 'dark' ? 'light' : 'dark'));
+  };
+
+  const toggleCustomTheme = () => {
+    setShowCustomTheme((prev) => !prev);
+  };
+
   const [workspaceName, setWorkspaceName] = useState('');
   const [workspaceDescription, setWorkspaceDescription] = useState('');
   const [error, setError] = useState('');
@@ -29,19 +96,30 @@ const WorkspaceCreation: React.FC = () => {
   };
 
   return (
-    <div className="workspace-creation-container">
-      <h1>Create Workspace</h1>
-      <div className="input-container">
-        <label>Workspace Name:</label>
-        <input type="text" value={workspaceName} onChange={handleWorkspaceNameChange} className="workspace-input" />
-      </div>
-      <div className="input-container">
-        <label>Workspace Description:</label>
-        <input type="text" value={workspaceDescription} onChange={handleWorkspaceDescriptionChange} className="workspace-input" />
-      </div>
-      {error && <p className="error-message">{error}</p>}
-      <button onClick={createWorkspace} className="create-workspace-button">Create Workspace</button>
-    </div>
+    <ThemeProvider theme={showCustomTheme ? LPtheme : defaultTheme}>
+    <CssBaseline />
+    <AppAppBar mode={mode} toggleColorMode={toggleColorMode} />
+    <Hero />
+      <Box sx={{ bgcolor: 'background.default' }}>
+        <div className="workspace-creation-container">
+          <h1>Create Workspace</h1>
+          <div className="input-container">
+            <label>Workspace Name:</label>
+            <input type="text" value={workspaceName} onChange={handleWorkspaceNameChange} className="workspace-input" />
+          </div>
+          <div className="input-container">
+            <label>Workspace Description:</label>
+            <input type="text" value={workspaceDescription} onChange={handleWorkspaceDescriptionChange} className="workspace-input" />
+          </div>
+          {error && <p className="error-message">{error}</p>}
+          <button onClick={createWorkspace} className="create-workspace-button">Create Workspace</button>
+        </div>
+    </Box>
+      <ToggleCustomTheme
+        showCustomTheme={showCustomTheme}
+        toggleCustomTheme={toggleCustomTheme}
+      />
+    </ThemeProvider>
   );
 };
 
